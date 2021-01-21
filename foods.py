caa36794e34c0819719baf7cc3f64b3b0d9c41ba
Mihail Marian <mihailmarian12@gmail.com>
2021-01-21 11:54:15 +0000

    foods

---
+++ foods.py
@@ -0,0 +1,13 @@
+"""List working"""
+
+my_foods = ["pizza", "falafel", "carrot cake"]
+friend_foods = my_foods[:]
+
+my_foods.append("cannoli")
+friend_foods.append("ice cream")
+
+print("favorite foods:")
+print(my_foods)
+
+print("\nfriend favorite foods:")
+print(friend_foods)
